c4f4d421b8808de551c1211012efdfce9de27f2f
Aldana Lafuente <44821947+aldilafuente@users.noreply.github.com>
2019-05-13 21:11:23 -0300

    Javascript practice

---
+++ practice2.js
@@ -0,0 +1,72 @@
+// Dada una variable "y" que puede contener un número de 1 a 7, determinar a que día de la semana corresponde. 
+// Incluir la opción si la variable toma un valor no válido (por ejemplo 0 u 8).
+// const day = 10;
+
+// if (day == 1) {
+//   console.log('Es lunes');
+// } else if (day == 2) {
+//   console.log('Es martes');
+// } else if (day == 3) {
+//   console.log('Es miércoles');
+// } else if (day == 4) {
+//   console.log('Es jueves');
+// } else if (day == 5) {
+//   console.log('Es viernes');
+// } else if (day == 6) {
+//   console.log('Es sábado');
+// } else if (day == 7) {
+//   console.log('Es domingo');
+// } else {
+//   console.log('El número ingresado no corresponde a ningún día');
+// }
+
+
+
+// Dada una variable donde se ingresa el nombre del mes, devolver la cantidad de días correspondiente.
+// const month = 'febrero';
+
+// if (month === 'enero' || month === 'marzo' || month === 'mayo' || month === 'julio' || month === 'agosto' || month === 'octubre' || month === 'diciembre') {
+//   console.log('El mes ingresado tiene 31 días');
+// } else if (month === 'abril' || month === 'junio' || month === 'septiembre' || month === 'noviembre') {
+//   console.log('El mes ingresado tiene 30 días');
+// } else if (month === 'febrero') {
+//   console.log('El mes ingresado tiene 28 días');
+// } else {
+//   console.log('El valor ingresado no corresponde a ningún mes, vuelva a intentarlo');
+// }
+
+
+
+// Dada una variable "x" la cual contiene un numero, determinar si ese numero es positivo o negativo
+// const number = 8;
+
+// if (number >= 0) {
+//   console.log('El número ingresado es positivo');
+// } else if (number < 0) {
+//   console.log('El número ingresado es negativo');
+// } else {
+//   console.log('El valor ingresado no es correcto');
+// }
+
+
+
+// Dada una variable "x" la cual contiene una letra, determinar si es una vocal.
+// const vowel = 'a';
+
+// if (vowel === 'a' || vowel === 'e' || vowel === 'i' || vowel === 'o' || vowel === 'u') {
+//   console.log('La letra ingresada es una vocal');
+// } else {
+//   console.log('La letra ingresada es una consonante');
+// }
+
+
+
+// Dadas dos variables ("x" e "y") las cuales contienen un numero, determinar cual es mayor.
+// const x = 10;
+// const y = 20;
+
+// if (x > y) {
+//   console.log('La variable X es mayor que la variable Y');
+// } else {
+//   console.log('La variable X es menor que la variable Y');
+// }
